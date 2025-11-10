80614d14e1e204259706c0c6c4be09daca125d3c
gabriel-1302 <142402787+gabriel-1302@users.noreply.github.com>
2025-11-09 22:47:28 -0400

    admin-funcion

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Codigo
 
+class CodigoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre_completo', 'funcion', 'habilitado', 'usado')
+
 # Register your models here.
-admin.site.register(Codigo)
+admin.site.register(Codigo, CodigoAdmin)
