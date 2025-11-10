9850ced976c259de8f38ea2893a4c9c7d4a34ff6
gabriel-1302 <142402787+gabriel-1302@users.noreply.github.com>
2025-11-09 21:52:22 -0400

    bd y modelo+funcion

--- api/models.py
+++ api/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 class Codigo(models.Model):
+    FUNCION_CHOICES = [
+        ('primera', 'Primera'),
+        ('segunda', 'Segunda'),
+    ]
     codigo = models.CharField(max_length=8, unique=True)
     nombre_completo = models.CharField(max_length=255, blank=True, null=True)
+    funcion = models.CharField(max_length=7, choices=FUNCION_CHOICES, default='primera', blank=False, null=False)
     habilitado = models.BooleanField(default=True)
     usado = models.BooleanField(default=False)
 
